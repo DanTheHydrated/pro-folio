017cc5ae1b6661086cdc3dc44926c37500949a80
Dan W <daniel.winn@student.awesomeinc.org>
2023-01-06 21:42:13 +0000

    this is an old version from gitpod

--- app/src/hero.js
+++ app/src/hero.js
@@ -4,7 +4,9 @@ import Dan from './images/Daniel_Winn.jpg/'
 export default function Hero() {
     return(
         <div className="container hero">
-            <img src={Dan} alt="image of me!" />
+            <img src={Dan} alt="image of me!" id="heroImg"/>
+            <h1> Hi! I'm Daniel Winn! </h1>
+            <h4> I'm a FullStack developer!</h4>
         </div>
     )
 }
