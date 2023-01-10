d7f9801ccbbed938407409feabdd476a4739dc97
Dan W <daniel.winn@student.awesomeinc.org>
2023-01-10 20:03:17 +0000

    working

--- app/src/hero.js
+++ app/src/hero.js
@@ -1,10 +1,14 @@
 import react from 'react'
 import Dan from './images/Daniel_Winn.jpg/'
+import Logo from './images/1664223925.901021-04F686CA-DB5B-4CE5-878D-74F6E7281126.png'
 
 export default function Hero() {
     return(
         <div className="container hero">
-            <img src={Dan} alt="image of me!" id="heroImg"/>
+            <Image className="social-icon" src = {Dan}
+            onMouseOver={e => e.currentTarget.src = Dan}
+            onMouseOut={e => e.currentTarget.src = Logo}
+            />
             <h1> Hi! I'm Daniel Winn! </h1>
             <h4> I'm a FullStack developer!</h4>
         </div>
